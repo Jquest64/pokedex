3a348306eca5bc7f9cb282c77b0a9aa27e11e079
Marcio Farias Pinto <marcio_fpinto@hotmail.com>
2023-01-26 10:52:13 -0300

    Final do projeto pokedex

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,8 +1,25 @@
 const pokeApi = {}
 
+function convertPokeApDrtailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.name = pokeDetail.name
+    pokemon.number = pokeDetail.id
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
     .then ((response) => response.json())
+    .then (convertPokeApDrtailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
